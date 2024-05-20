713c5a44c0b007aa73af20cfac4906c8a9dbde27
Sebastian Schmitt <schmitse@sebastians-air.speedport.ip>
2024-05-20 18:44:46 +0200

    add progressbar to cpp code

--- cpp/targets/perlin.cpp
+++ cpp/targets/perlin.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <fstream>
 #include <CLI/CLI.hpp>
+#include <indicators/progress_bar.hpp>
+
 
 int main(int argc, char **argv) {
     // Default values
@@ -16,7 +18,7 @@ int main(int argc, char **argv) {
     CLI::App app{"Perlin Noise Generator"};
     
     app.add_option("-H,--height", height, "Height of the grid")->default_val("1080");
-    app.add_option("-W,--width", width, "Width of the grid")->default_val("1920");
+    app.add_option("-w,--width", width, "Width of the grid")->default_val("1920");
     app.add_option("-s,--seed", seed, "Seed for the noise generator")->default_val("1337");
     app.add_option("-g,--grid_size", grid_size, "Grid size for the noise")->default_val("400.0");
     app.add_option("-o,--octaves", octaves, "Number of octaves for the noise")->default_val("8");
@@ -28,6 +30,21 @@ int main(int argc, char **argv) {
     float frequency = 1.0;
     std::vector<float> values((height * width), 0.0);
     Perlin noiseCreator = Perlin(seed);
+
+    indicators::ProgressBar bar{
+        indicators::option::BarWidth{40},
+        indicators::option::Start{"["},
+        indicators::option::Fill{"="},
+        indicators::option::Lead{">"},
+        indicators::option::Remainder{" "},
+        indicators::option::End{"]"},
+        indicators::option::PostfixText{"Generating noise..."},
+        indicators::option::ForegroundColor{indicators::Color::cyan},
+        indicators::option::ShowElapsedTime{true},
+        indicators::option::ShowRemainingTime{true},
+        indicators::option::ShowPercentage{true},
+        indicators::option::MaxProgress{width}
+    };
 
     for (int x = 0; x < width; x++) {
         for (int y = 0; y < height; y++) {
@@ -45,7 +62,11 @@ int main(int argc, char **argv) {
             // values.push_back(val);
             values[index] = val;
         }
+        bar.tick();
     }
+    bar.set_option(indicators::option::PostfixText{"Noise generation complete!"});
+    bar.mark_as_completed();
+
     std::vector<float> result = noiseCreator.rescale(values);
     std::cout << "Had a total of " << noiseCreator.getCacheHits() << " cache hits." << std::endl;
 
